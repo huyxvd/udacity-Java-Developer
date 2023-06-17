c3c422afa2ffad3f6c434010fd5319cbba03ca28
Huy Dep Trai <huyxvd@gmail.com>
2023-06-17 23:25:43 +0700

    update the admin menu

---
+++ Project1-Hotel-Reservation-Application/src/AdminMenu.java
@@ -0,0 +1,180 @@
+import api.AdminResource;
+import api.HotelResource;
+import model.Customer;
+import model.IRoom;
+import model.Room;
+import model.RoomType;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Scanner;
+
+public class AdminMenu {
+    private static AdminMenu _AdminMenu;
+    private AdminMenu() {
+    }
+
+    public static AdminMenu getInstance() {
+        if(_AdminMenu == null) {
+            _AdminMenu = new AdminMenu();
+        }
+
+        return _AdminMenu;
+    }
+
+    private void DisplayAdminMenu()
+    {
+        System.out.println("Admin Menu:");
+        System.out.println("------------------------------------");
+        System.out.println("1.  See all Customers");
+        System.out.println("2.  See all Rooms");
+        System.out.println("3.  See all Reservations");
+        System.out.println("4.  Add a Room");
+        System.out.println("5.  Fake data");
+        System.out.println("6.  back to main menu");
+        System.out.println("------------------------------------");
+        System.out.print("Enter your choice: ");
+    }
+
+    public void InputRoom() {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean addMoreRooms = true;
+
+        while (addMoreRooms) {
+            System.out.print("Enter room number: ");
+            String roomNumber = scanner.nextLine();
+
+            System.out.print("Enter price: ");
+            double price = scanner.nextDouble();
+            scanner.nextLine(); // Consume the newline character
+
+            System.out.print("Enter room type (1 - SINGLE, 2 or any - DOUBLE,): ");
+            String roomTypeString = scanner.nextLine();
+            int roomT = Integer.parseInt(roomTypeString);
+
+            RoomType roomType;
+            if(roomT == 1) {
+                roomType = RoomType.SINGLE;
+            } else {
+
+                roomType = RoomType.SINGLE;
+            }
+
+            // Create an instance of the Room class with the entered information
+            Room room = new Room(roomNumber, price, roomType);
+            //.add(room);
+            AdminResource _AdminResource = AdminResource.getInstance();
+            _AdminResource.addRoom(room);
+
+            System.out.print("Add another room? (y/n): ");
+            String addAnotherRoom = scanner.nextLine();
+            addMoreRooms = addAnotherRoom.equalsIgnoreCase("y");
+        }
+    }
+
+    public void AdminMenuOnConsole() {
+
+        AdminResource _AdminResource = AdminResource.getInstance();
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        do {
+            try {
+                DisplayAdminMenu();
+                option = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid option number.");
+                scanner.nextLine(); // Clear the invalid input from the scanner
+                option = 0; // Set option to 0 to continue the loop
+            }
+
+            switch (option) {
+                case 1:
+                    // Perform actions for Option 1
+                    System.out.println("-----------See all Customers---------------");
+                    for (Customer element : _AdminResource.getAllCustomers()) {
+                        element.displayInfo();
+                    }
+                    break;
+                case 2:
+                    // Perform actions for Option 2
+                    System.out.println("-----------See all Rooms---------------");
+                    for (IRoom element : _AdminResource.getAllRooms()) {
+                        element.displayInfo();
+                    }
+                    break;
+                case 3:
+                    // Perform actions for Option 3
+                    System.out.println("-----------See all Reservations---------------");
+                    _AdminResource.displayAllReservations();
+                    break;
+                case 4:
+                    System.out.println("4.  Add a Room");
+                    // Perform actions for Option 4
+                    InputRoom();
+                    break;
+                case 5:
+                    System.out.println("5.  fake some room");
+                    try{
+                        FakeData();
+                    } catch (Exception ex) {
+
+                        System.out.println("in valid data");
+                        System.out.println(ex.getMessage());
+                    }
+                    break;
+                case 6:
+                    System.out.println("6.  Back to Main Menu");
+                    break;
+                default:
+                    System.out.println("Invalid option! Please choose a valid option.");
+                    break;
+            }
+
+            System.out.println();
+        } while (option != 6);
+
+        System.out.println("Admin menu exited.");
+    }
+
+    private void FakeData() throws ParseException {
+        AdminResource _AdminResource = AdminResource.getInstance();
+        Room room1 = new Room("101", 100.0, RoomType.SINGLE);
+        Room room2 = new Room("102", 150.0, RoomType.DOUBLE);
+        Room room3 = new Room("103", 250.0, RoomType.DOUBLE);
+        Room room4 = new Room("104", 350.0, RoomType.DOUBLE);
+        _AdminResource.addRoom(room1);
+        _AdminResource.addRoom(room2);
+        _AdminResource.addRoom(room3);
+        _AdminResource.addRoom(room4);
+
+        HotelResource _HotelResource = HotelResource.getInstance();
+        _HotelResource.createACustomer("john.doe@example.com", "John", "Doe");
+        _HotelResource.createACustomer("alice.smith@example.com", "Alice", "Smith");
+        _HotelResource.createACustomer("bob.johnson@example.com", "Bob", "Johnson");
+        _HotelResource.createACustomer("emily.davis@example.com", "Emily", "Davis");
+        _HotelResource.createACustomer("michael.brown@example.com", "Michael", "Brown");
+
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        try {
+            _HotelResource.bookARoom("john.doe@example.com", room1, dateFormat.parse("1-1-2000"), dateFormat.parse("10-1-2000") );
+            _HotelResource.bookARoom("john.doe@example.com", room2, dateFormat.parse("1-1-2000"), dateFormat.parse("10-1-2000") );
+            Date in = dateFormat.parse("5-1-2000");
+            Date out = dateFormat.parse("6-1-2000");
+            Collection<IRoom> rooms = _HotelResource.findARoom( in, out );
+
+            for (IRoom room: rooms) {
+                room.displayInfo();
+            }
+        } catch (Exception a) {
+
+            System.out.println("pproblem when seed data");
+            System.out.println(a.getMessage());
+        }
+    }
+}
---
+++ Project1-Hotel-Reservation-Application/src/MainMenu.java
@@ -0,0 +1,153 @@
+import api.HotelResource;
+import model.IRoom;
+import model.Reservation;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Scanner;
+
+public class MainMenu {
+
+    private static MainMenu _MainMenu;
+    private static final AdminMenu _AdminMenu = AdminMenu.getInstance();
+    private MainMenu() {
+    }
+
+    public static MainMenu getInstance() {
+        if(_MainMenu == null) {
+            _MainMenu = new MainMenu();
+        }
+
+        return _MainMenu;
+    }
+
+    public void DisplayMainMenu() {
+        System.out.println("Menu:");
+        System.out.println("------------------------------------");
+        System.out.println("1.  Find and reserve a room");
+        System.out.println("2.  See my reservations");
+        System.out.println("3.  Create an account");
+        System.out.println("4.  Admin");
+        System.out.println("5.  Exit");
+        System.out.println("------------------------------------");
+        System.out.print("Enter your choice: ");
+    }
+
+    public void MainMenuOnConsole(int option) {
+        HotelResource _HotelResource = HotelResource.getInstance();
+        Scanner scanner = new Scanner(System.in);
+        switch (option) {
+            case 1:
+                System.out.println("------------------Find and reserve a room------------------");
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+                System.out.print("Enter check-in date (dd-MM-yyyy): ");
+                String checkInDateStr = scanner.nextLine();
+
+                System.out.print("Enter check-out date (dd-MM-yyyy): ");
+                String checkOutDateStr = scanner.nextLine();
+                Date checkInDate = new Date();
+                Date checkOutDate = new Date();
+
+                try {
+                    checkInDate = dateFormat.parse(checkInDateStr);
+                    checkOutDate = dateFormat.parse(checkOutDateStr);
+
+                    Collection<IRoom> rooms = _HotelResource.findARoom(checkInDate, checkOutDate);
+
+                    if(rooms.isEmpty()) {
+                        System.out.println("no room available in search range we suggest other range ");
+
+                        // Create a Calendar instance and set the Date object
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTime(checkOutDate);
+
+                        // Increase the date by a specific amount
+                        calendar.add(Calendar.DAY_OF_MONTH, 30);
+
+                        // Get the increased date as a Date object
+                        Date increasedDate = calendar.getTime();
+                        calendar.add(Calendar.DAY_OF_MONTH, 35);
+                        Date increasedDate2 = calendar.getTime();
+                        System.out.println("From: " + increasedDate + "To: " + increasedDate2 );
+                        rooms = _HotelResource.findARoom(increasedDate, increasedDate2);
+                        if(rooms.isEmpty()) {
+                            break;
+                        }
+                    }
+
+                    for (IRoom room: rooms) {
+                        room.displayInfo();
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Invalid date format. Please enter dates in the format dd-MM-yyyy.");
+                }
+
+                System.out.print("which room you want to reserve: ");
+                String roomIdReserver = scanner.nextLine();
+                IRoom room = _HotelResource.getRoom(roomIdReserver);
+                System.out.print("customer email: ");
+                String _customerEmail = scanner.nextLine();
+
+                Reservation res = _HotelResource.bookARoom(_customerEmail, room, checkInDate, checkOutDate );
+                res.displayInfo();
+
+                // Perform actions for Option 1
+                break;
+            case 2:
+                System.out.println("--------------------See my reservations---------------------");
+                System.out.println("customer email: ");
+                String customerEmail = scanner.nextLine();
+                Collection<Reservation> reservations = _HotelResource.getCustomersReservations(customerEmail);
+
+                if(reservations.isEmpty()) {
+                    System.out.println("no reservations found");
+                    break;
+                }
+
+                for (Reservation element: reservations) {
+                    element.displayInfo();
+                }
+                // Perform actions for Option 2
+                break;
+            case 3:
+                System.out.println("-----------------------------Create an account---------------------------");
+
+                System.out.println("Enter customer information:");
+
+                String firstName = "";
+                String lastName = "";
+                String email = "";
+
+                System.out.print("First Name: ");
+                firstName = scanner.nextLine();
+
+                System.out.print("Last Name: ");
+                lastName = scanner.nextLine();
+
+                System.out.print("Email: ");
+                email = scanner.nextLine();
+
+                _HotelResource.createACustomer(email, firstName, lastName);
+                // Perform actions for Option 3
+                break;
+            case 4:
+                System.out.println("-------------open admin menu----------------");
+                // calling admin menu
+                _AdminMenu.AdminMenuOnConsole();
+                // Perform actions for Option 4
+                break;
+            case 5:
+                System.out.println("5.  Exit");
+                break;
+            default:
+                System.out.println("Invalid option! Please choose a valid option.");
+                break;
+        }
+    }
+
+
+}
